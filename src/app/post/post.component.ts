c5c68c54cc077d4e793893dfdbbdf2fbbfb25d6e
kalwitaloibiapina <kalwitaloibiapina@gmail.com>
2019-06-26 00:59:13 -0300

    add glide

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PostService} from '../service/postService';
 import {Post} from '../model/Post';
+import Glide from '@glidejs/glide';
 
 @Component({
   selector: 'app-post',
@@ -10,15 +11,38 @@ import {Post} from '../model/Post';
 export class PostComponent implements OnInit {
 
   public posts: Post[] = [];
-  constructor(private postService: PostService) { }
+
+  constructor(private postService: PostService) {
+  }
 
   ngOnInit() {
+    /* Get the element you want displayed in fullscreen */
+    new Glide('.glide', {
+      type: 'carousel',
+      startAt: 0,
+      autoplay: 5000,
+    }).mount();
+  }
 
-    const elem = document.getElementById('testando');
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen();
+
+  openFullScreen(): void {
+    const elem = document.getElementById('carousel');
+
+    const docElmWithBrowsersFullScreenFunctions = elem as HTMLElement & {
+      mozRequestFullScreen(): Promise<void>;
+      webkitRequestFullscreen(): Promise<void>;
+      msRequestFullscreen(): Promise<void>;
+    };
+
+    if (docElmWithBrowsersFullScreenFunctions.requestFullscreen) {
+      docElmWithBrowsersFullScreenFunctions.requestFullscreen();
+    } else if (docElmWithBrowsersFullScreenFunctions.mozRequestFullScreen) { /* Firefox */
+      docElmWithBrowsersFullScreenFunctions.mozRequestFullScreen();
+    } else if (docElmWithBrowsersFullScreenFunctions.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+      docElmWithBrowsersFullScreenFunctions.webkitRequestFullscreen();
+    } else if (docElmWithBrowsersFullScreenFunctions.msRequestFullscreen) { /* IE/Edge */
+      docElmWithBrowsersFullScreenFunctions.msRequestFullscreen();
     }
-
   }
 
   findPostByHashtag(hashtag: string): void {
